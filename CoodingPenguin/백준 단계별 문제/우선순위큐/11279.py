b6af36e2777e8182c9ef2a46b8f0a67e4b7ed92c
CoodingPenguin <coodingpenguin@gmail.com>
2021-02-21 21:25:46 +0900

    백준 우선순위큐 3문제

---
+++ CoodingPenguin/백준 단계별 문제/우선순위큐/11279.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+for _ in range(n):
+  x = int(input())
+  if x:
+    heapq.heappush(q, -x)
+  else:
+    if q:
+      print(-heapq.heappop(q))
+    else:
+      print(0)
